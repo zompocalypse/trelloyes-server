71e449ec1f11b3ebe105a93acb20abbf3e7729d5
Jason Stankevich <jason.stankevich@gmail.com>
2020-09-17 20:35:52 -0400

    server assignment completedsetup walkthrough completed

---
+++ src/store.js
@@ -0,0 +1,13 @@
+const cards = [{
+  id: 1,
+  title: 'Task One',
+  content: 'This is card one'
+}];
+
+const lists = [{
+  id: 1,
+  header: 'List One',
+  cardIds: [1]
+}];
+
+module.exports = { cards, lists };
